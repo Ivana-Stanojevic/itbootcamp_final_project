828e20fa418421fb81e0fb1198b91687c72bcb51
Ivana-Stanojevic <stnojeviciva89@gmail.com>
2023-01-31 18:40:09 +0100

    Base Test initialised

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait driverWait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com/";
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("driver.chrome.driver", "F:\\IVANA KOMP\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @BeforeMethod
+    public void beforMethod() {
+        driver.get(baseUrl);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
