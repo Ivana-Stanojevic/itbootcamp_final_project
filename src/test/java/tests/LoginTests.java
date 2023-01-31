7922b0d51aabde6b2b0ff82cfd4d2c74fd8e9f0e
Ivana-Stanojevic <stnojeviciva89@gmail.com>
2023-01-31 19:30:02 +0100

    Test 2: Checks input types

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,23 +4,52 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends BaseTest {
-    HomePage homePage;
+   private HomePage homePage;
+    private LoginPage loginPage;
 
     @BeforeClass
     @Override
     public void beforeClass() {
         super.beforeClass();
         homePage=new HomePage(driver,driverWait);
+        loginPage=new LoginPage(driver, driverWait);
     }
 
     @Test
     public void visitsTheLoginPage() {
+
         homePage.loginButtonClick();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         String actualUrl=driver.getCurrentUrl();
         String expectedUrl ="https://vue-demo.daniel-avellaneda.com/login";
         Assert.assertEquals(actualUrl, expectedUrl);
+
+    }
+
+    @Test
+    public void checksInputTypes() {
+        homePage.loginButtonClick();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String actualEmailType=loginPage.getEmail().getAttribute("type");
+        String expectedEmailType= "email";
+        Assert.assertEquals(actualEmailType, expectedEmailType);
+
+        String actualPasswordType=loginPage.getPassword().getAttribute("type");
+        String expectedPasswordType="password";
+        Assert.assertEquals(actualPasswordType, expectedPasswordType);
 
     }
 }
