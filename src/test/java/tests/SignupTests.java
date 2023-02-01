6dca5ac56a4e04925ad81b5deafbeb9c7b4f63c0
Ivana-Stanojevic <stnojeviciva89@gmail.com>
2023-02-01 17:39:01 +0100

    Signup Tests Test 2: Checks input types

--- src/test/java/tests/SignupTests.java
+++ src/test/java/tests/SignupTests.java
@@ -1,20 +1,25 @@
 package tests;
 
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.SignupPage;
 
 public class SignupTests extends BaseTest {
 
     private HomePage homePage;
+    private SignupPage signupPage;
 
     @BeforeClass
     @Override
     public void beforeClass() {
         super.beforeClass();
         homePage = new HomePage(driver, driverWait);
+        signupPage = new SignupPage(driver, driverWait);
     }
 
     @BeforeMethod
@@ -30,5 +35,21 @@ public class SignupTests extends BaseTest {
         String actualSignup = driver.getCurrentUrl();
         String expectSignup = baseUrl + "/signup";
         Assert.assertEquals(actualSignup, expectSignup);
+    }
+
+    @Test
+    public void checksInputTypes() {
+        Assert.assertEquals(signupPage.getEmail().getAttribute("type"), "email");
+        Assert.assertEquals(signupPage.getPassword().getAttribute("type"), "password");
+        Assert.assertEquals(signupPage.getConfirmPassword().getAttribute("type"), "password");
+
+    }
+
+    @Test
+    public void displaysErrorsWhenUserAlreadyExists() {
+        signupPage.fillSignUp("Test Test", "admin@admin.com", "123654", "123654");
+        driverWait.until(ExpectedConditions.visibilityOf(signupPage.getMessage()));
+        Assert.assertEquals(signupPage.getMessage().getText(), "E-mail already exists");
+        Assert.assertEquals(driver.getCurrentUrl(), baseUrl+"/signup");
     }
 }
